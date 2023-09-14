02070dc1721c66923bb7e54ef9ca52748209f1eb
422UR4H <tiagosindra@id.uff.br>
2023-09-14 00:34:43 -0300

    :sparkles: fix/database

--- PoC_TypeScript_DB.sql
+++ PoC_TypeScript_DB.sql
@@ -1,9 +1,12 @@
 CREATE TABLE players (
     id SERIAL PRIMARY KEY,
     nick VARCHAR(16) NOT NULL UNIQUE,
+    email VARCHAR(64) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     name VARCHAR(32) NOT NULL,
     description VARCHAR(255),
     birthday TIMESTAMP NOT NULL,
     "avatarUrl" TEXT,
-    "createdAt" TIMESTAMP DEFAULT NOW()
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "updatedAt" TIMESTAMP
 );
