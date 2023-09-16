c19cc211f36f21cd69f0ea4925ccfc3508d14f36
422UR4H <tiagosindra@id.uff.br>
2023-09-16 01:21:42 -0300

    :bug: fix/update-player

--- src/routers/player.routes.ts
+++ src/routers/player.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import validateAuth from "@/middlewares/validateAuth";
 import validateSchema from "@/middlewares/validateSchema";
-import { playerSchema } from "@/schemas/player.schemas";
+import { updatePlayerSchema } from "@/schemas/player.schemas";
 import { update, deleteById } from "@/controllers/player.controllers";
 
 const router = Router();
 
-router.put("/api/players", validateAuth, validateSchema(playerSchema), update);
+router.put("/api/players", validateAuth, validateSchema(updatePlayerSchema), update);
 router.delete("/api/players", validateAuth, deleteById);
 
 export default router;
--- src/schemas/player.schemas.ts
+++ src/schemas/player.schemas.ts
@@ -4,7 +4,7 @@ import JoiDate from "@joi/date";
 
 const Joi = JoiBase.extend(JoiDate) as Root;
 
-export const playerSchema = Joi.object<CreatePlayer | UpdatePlayer>({
+export const playerSchema = Joi.object<CreatePlayer>({
     nick: Joi.string().min(4).max(16).required(),
     name: Joi.string().min(4).max(32).required(),
     email: Joi.string().email().min(9).max(64).required(),
@@ -12,4 +12,8 @@ export const playerSchema = Joi.object<CreatePlayer | UpdatePlayer>({
     description: Joi.string().max(255),
     avatarUrl: Joi.string().uri(),
     birthday: Joi.date().format('DD-MM-YYYY').less('now').required()
+});
+
+export const updatePlayerSchema = Joi.object<UpdatePlayer>({
+
 });
